7acfaa3a9c4d1ce9c4ad230418aa5cefab43f718
Michael Newman <40316353+Newms226@users.noreply.github.com>
2018-10-16 21:09:08 -0700

    basic structure

--- src/main.s
+++ src/main.s
@@ -1,54 +1,48 @@
-; main.s
-; Runs on any Cortex M processor
-; A very simple first project implementing a random number generator
-; Daniel Valvano
-; May 12, 2015
+; r12 := key
+; r11 := recieved word
+; r10
+; r9
+; r8
+; r7  := case register for jump table
 
-;  This example accompanies the book
-;  "Embedded Systems: Introduction to Arm Cortex M Microcontrollers",
-;  ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2015
-;  Section 3.3.10, Program 3.12
-;
-;Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
-;   You may use, edit, run or distribute this file
-;   as long as the above copyright notice remains
-;THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
-;OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
-;MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
-;VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
-;OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
-;For more information about my classes, my research, and my books, see
-;http://users.ece.utexas.edu/~valvano/
+		THUMB
+        AREA    |.text|, CODE, READONLY, ALIGN=2
+        EXPORT  Start
+			
 
 
-; we align 32 bit variables to 32-bits
-; we align op codes to 16 bits
-       THUMB
-       AREA    DATA, ALIGN=4 
-       EXPORT  M [DATA,SIZE=4]
-M      SPACE   4
-                 
-       AREA    |.text|, CODE, READONLY, ALIGN=2
-       EXPORT  Start
-Start  LDR R2,=M       ; R2 = &M, R2 points to M
-       MOV R0,#1       ; Initial seed
-       STR R0,[R2]     ; M=1
-loop   BL  Random
-       B   loop
-;------------Random------------
-; Return R0= random number
-; Linear congruential generator 
-; from Numerical Recipes by Press et al.
-Random LDR R2,=M    ; R2 = &M, R2 points to M
-       LDR R0,[R2]  ; R0=M
-       LDR R1,=1664525
-       MUL R0,R0,R1 ; R0 = 1664525*M
-       LDR R1,=1013904223
-       ADD R0,R1    ; 1664525*M+1013904223 
-       STR R0,[R2]  ; store M
-       BX  LR
+key		DCD		0x37A2B89E
+message	DCD		0x
 
+Start
+		ADR		r12, key
+		LDR		r12, [r12]
+		ADR		r11, message
+		LDR		r11, message
 
-       ALIGN      
-       END  
-           
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+encrpyt
+
+decrypt
+
+;~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+JumpTable
+		DCD		case_0
+		DCD		case_1
+		DCD		case_2
+		DCD		case_3
+		
+load_case
+		MOVS	r0, r12
+		MOVPL
+
+case_0
+
+case_1
+
+case_2
+
+case_3
+		
+		ALIGN
+		END
